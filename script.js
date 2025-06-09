9ad4b43ba251f9a711027413f55cb706c521d428
Saurav <jhasaurav593@gmail.com>
2025-06-09 13:38:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,19 @@
 const makeChange = (c) => {
   // your name here
+  const changes = {};
+
+  changes.q = Math.floor(c / 25);
+  c = c % 25;
+
+  changes.d = Math.floor(c / 10);
+	c = c % 10;
+
+  changes.n = Math.floor(c / 5);
+	c = c % 5;
+
+  changes.p = c;
+
+  return changes;
 };
 
 // Do not the change the code below
